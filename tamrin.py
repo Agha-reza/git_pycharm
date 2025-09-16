d533397fe564fb3fa34f1e3f5b6543ffefc2fe87
Aghareza <rezaalizadeh84129@gmail.com>
2025-09-16 17:08:27 +0330

    therd

--- tamrin.py
+++ tamrin.py
@@ -120,7 +120,4 @@ def sign_in():
         else:
             print("your password or user is not correct")
 
-k = Karmand() # لزوم ساخت یک شی کلاس پنل کارمند بصورت مستقل
-dic = {} # اطلاعات پنل1
-#Unauthorized
-# salaaaaaaam
+
